c24ada14f0672ec0bfec4309b0e62668f82bf930
EddwardCuadra <41312733+EddwardCuadra@users.noreply.github.com>
2018-08-03 14:33:58 -0500

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = $_POST['nombre'];
+$cedula = $_POST['cedula'];
+$sexo = $_POST['sexo'];
+$aficion = $_POST['aficion'];
+$povincia = $_POST['provincia'];
+$fecha = $_POST['user_date'];
+echo "$nombre";
+echo "$cedula";
+echo "$sexo";
+echo "$aficion";
+echo "$provincia";
+echo "$fecha";
