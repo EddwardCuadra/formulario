c1a2c170fc8012ccf1ae228b64ba11cbddeabab8
EddwardCuadra <41312733+EddwardCuadra@users.noreply.github.com>
2018-08-03 14:44:49 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,14 +1,4 @@
 <?php
 $nombre = $_POST['nombre'];
-$cedula = $_POST['cedula'];
-$sexo = $_POST['sexo'];
-$aficion = $_POST['aficion'];
-$povincia = $_POST['provincia'];
-$fecha = $_POST['user_date'];
 echo "<p>$nombre</p>";
-echo "<p>$cedula</p>";
-echo "<p>$sexo</p>";
-echo "<p>$aficion</p>";
-echo "<p>$provincia</p>";
-echo "<p>$fecha</p>";
 ?>
